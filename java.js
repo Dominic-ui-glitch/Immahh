236fb114c1a430a4cbaaf6a01fb1e28e8c7bb4f6
Dominic-ui-glitch <musyokad978@gmail.com>
2026-08-22 04:01:44 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,9 @@
 function handleYes() {
+  const music = document.getElementById('bg-music');
+  if (music) music.play().catch(err => console.log(err));
+
   const responseArea = document.getElementById('response-area');
-  responseArea.innerHTML = '<p class="feedback-msg" style="color: #ff4d6d;">YAY 🥹😛 ❤️</p>';
+  responseArea.innerHTML = '<p class="feedback-msg" style="color: #ff4d6d;">YAY 🥺😛</p>';
 
   setTimeout(() => {
     document.getElementById('question-screen').classList.add('hidden');
